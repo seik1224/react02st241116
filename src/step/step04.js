23552f932c4ed4374d9badec4337b8732f8c8208
seik <seik1224@naver.com>
2024-11-16 15:00:09 +0900

    .

--- src/step/step04.js
+++ src/step/step04.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
+/*
+    useRef사용법 4. 토글상태관리
+*/
 const Step04 = () => {
+    const toggleRef = useRef(false);
+    const [isVi, setIsVi] = useState(false);
+
+    const toggleVi = () => {
+        toggleRef.current = !toggleRef.current;
+        setIsVi(toggleRef.current);
+    }
   return (
-    <div>Step04</div>
+    <>
+        <button onClick={toggleVi}>토글</button>
+        {isVi && <div>표시됨</div>}
+    </>
   )
 }
 
---
+++ src/step/step05.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+/*
+    [ useEffect ]
+    컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록
+    할 수 있는 HOOK
+    함수와 비슷한데, 함수는 실행되는 시점이 정해져있지만
+    useEffect는 컴포넌트가 렌더링될 때마다 실행
+*/
+
+const Step05 = () => {
+  return (
+    <div>Step05</div>
+  )
+}
+
+export default Step05
